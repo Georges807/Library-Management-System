292ca3bebc2d6abff3176a4b9213c9fbb0743628
Georges807 <128749024+Georges807@users.noreply.github.com>
2024-12-08 21:28:23 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Exam_arshath
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
